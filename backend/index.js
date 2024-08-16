62cbd98a919be13fc7cb96053fb7e3985132ba6b
Palermo <124798752+hpe-palermo@users.noreply.github.com>
2024-08-16 19:29:26 -0300

    Add files via upload

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const router = require('./router');
+const mysql = require('mysql');
 
 const app = express();
 const port = 3000;
@@ -8,6 +9,23 @@ const port = 3000;
 app.use(cors());
 app.use(express.json());
 app.use('/', router);
+
+const conn = mysql.createConnection({
+    host: "localhost",
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
+});
+
+conn.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected to MySQL!");
+
+    // conn.query("CREATE DATABASE mydb", function (err, result) {
+    //     if (err) throw err;
+    //     console.log("Database created");
+    // });
+});
 
 app.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}`);
