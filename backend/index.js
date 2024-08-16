298ff2c14f6ff41e137dd62affe37f637dab4fcc
hpe-palermo <hpalermoemerick@gmail.com>
2024-08-15 22:01:50 -0300

    api funfando

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const cors = require("cors");
 const router = require('./router');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/", router);
 app.use(cors());
 app.use(express.json());
--- backend/router.js
+++ backend/router.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const router = express.Router();
 
-let users = ["Henrique", "Paulo"];
+let users = [
+    {
+        nome: "Henrique",
+        email: "henrique@gmail.com"
+    },
+    {
+        nome: "Paulo",
+        email: "paulo@gmail.com"
+    }
+];
 
 router.get("/", (req, res) => {
     res.send("<h1>API on</h1>");
@@ -14,6 +23,35 @@ router.get("/users", (req, res) => {
 router.get("/users/:id", (req, res) => {
     const { id } = req.params;
     res.json({ users: users[id] });
+});
+
+router.post("/create", (req, res) => {
+    const { nome, email } = req.body;
+    users.push({ nome, email });
+    res.send("Usuário adicionado!!!\n")
+});
+
+router.put("/users/:id", (req, res) => {
+    const { id } = req.params;
+    const { nome, email } = req.body;
+
+    if (id >= 0 && id < users.length) {
+        users[id] = { nome, email };
+        res.send("Usuário atualizado!!!");
+    } else {
+        res.status(404).send("Usuário não encontrado.");
+    }
+});
+
+router.delete("/users/:id", (req, res) => {
+    const { id } = req.params;
+
+    if (id >= 0 && id < users.length) {
+        users.splice(id, 1);
+        res.send("Usuário removido!!!");
+    } else {
+        res.status(404).send("Usuário não encontrado.");
+    }
 });
 
 module.exports = router;
