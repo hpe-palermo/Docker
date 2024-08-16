55d4afdc31d2dbde8b826ba31a3a6e0a3bed8f38
Palermo <124798752+hpe-palermo@users.noreply.github.com>
2024-08-16 19:55:50 -0300

    Add files via upload

--- init/init.sql
+++ init/init.sql
@@ -4,11 +4,11 @@ CREATE DATABASE IF NOT EXISTS dbteste;
 USE dbteste;
 
 -- Cria uma tabela de exemplo
-CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE IF NOT EXISTS Usuario (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE
 );
 
 -- Insere dados de exemplo
-INSERT INTO users (nome, email) VALUES ('John Doe', 'john.doe@example.com');
+INSERT INTO Usuario (nome, email) VALUES ('John Doe', 'john.doe@example.com');
